1698a6c31ad25e34b58fe0745b3fb08f41302dad
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-03 21:14:15 -0700

    Appcolor added

---
+++ Africa/AfricaTests/AfricaTests.swift
@@ -0,0 +1,19 @@
+//
+//  AfricaTests.swift
+//  AfricaTests
+//
+//  Created by Dhruv Patel on 02/07/26.
+//
+
+import Testing
+@testable import Africa
+
+struct AfricaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
