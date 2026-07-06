91700664f30cd1a310cf9137d5d409f59f37d67c
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-05 22:16:51 -0700

    Contentview updated and folder structure changed.

---
+++ Africa/Africa/App/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Dhruv Patel on 02/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    var body: some View {
+        Text("Content").padding()
+    }
+
+   
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewDevice("iPhone 11 Pro")
+    }
+}
