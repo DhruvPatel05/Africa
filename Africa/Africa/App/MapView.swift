abc8a3f7558537cc724de720df6ee1d5e0d0e581
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-05 22:15:56 -0700

    MapView Added.

---
+++ Africa/Africa/App/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Dhruv Patel on 05/07/26.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+#Preview {
+    MapView()
+}
