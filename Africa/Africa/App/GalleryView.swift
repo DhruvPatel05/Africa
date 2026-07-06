32073574fda070c7858fb6360e0f54fa80a16479
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-06 12:20:46 -0700

    Galleryview for tabitem added

---
+++ Africa/Africa/App/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by Dhruv Patel on 06/07/26.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryView()
+}
