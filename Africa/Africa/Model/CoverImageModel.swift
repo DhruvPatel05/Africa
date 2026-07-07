20f4b2c443df95d9da23011b1fa92dcee49e1e6f
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-07 12:38:04 -0700

    CoverImageModel Added.

---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,15 @@
+
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Dhruv Patel on 07/07/26.
+//
+
+import SwiftUI
+
+struct CoverImage : Codable,Identifiable {
+    let id: Int
+    let name: String
+}
+
