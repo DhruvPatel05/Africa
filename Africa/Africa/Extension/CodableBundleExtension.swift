cf2928c6dc9fefb24a0b9f63198e5818d3fe1ef1
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-07 13:11:46 -0700

    Codable Extension added

---
+++ Africa/Africa/Extension/CodableBundleExtension.swift
@@ -0,0 +1,33 @@
+//
+//  Untitled.swift
+//  Africa
+//
+//  Created by Dhruv Patel on 07/07/26.
+//
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [CoverImage] {
+        // 1. Locate JSON File
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
+        
+        // 2. Create a Property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle")
+        }
+        
+        // 3. Create a decoder
+        let decoder = JSONDecoder()
+        
+        // 4. Create a property for the decode data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        // 5. Return the ready to Use Data
+
+        return loaded
+        }
+}
+
