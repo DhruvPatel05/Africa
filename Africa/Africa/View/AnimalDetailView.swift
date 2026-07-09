e6f5f8a59b97270e5e2e6099147ad5d56ba02cfb
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-09 14:15:45 -0700

    AnimalDetailview updated using weblinkview

--- Africa/Africa/View/AnimalDetailView.swift
+++ Africa/Africa/View/AnimalDetailView.swift
@@ -66,7 +66,10 @@ struct AnimalDetailView: View {
                 }
                 .padding(.horizontal)
                 // LINK
-                
+                Group {
+                    HeadingView(headingImage: "books.vertical", headingText: "Learn More")
+                    ExternalWeblinkView(animal: animal)
+                }
                 
             }//: VSTACK
             .navigationBarTitle("Learn about \(animal.name)",displayMode: .inline)
