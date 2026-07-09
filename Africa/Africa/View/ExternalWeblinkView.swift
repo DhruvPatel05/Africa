f54e7bdafddd0df6dd22df6659d93989fb7d5cc8
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-09 14:15:12 -0700

    Weblink view added.

---
+++ Africa/Africa/View/ExternalWeblinkView.swift
@@ -0,0 +1,41 @@
+//
+//  ExternalWeblinkView.swift
+//  Africa
+//
+//  Created by Dhruv Patel on 09/07/26.
+//
+
+import SwiftUI
+
+struct ExternalWeblinkView: View {
+    
+    // MARK: - PROPERTIES
+    let animal: Animal
+    
+    // MARK: - BODY
+    var body: some View {
+        GroupBox {
+            HStack {
+                Image(systemName: "globe")
+                Text("Wikipedia")
+                Spacer()
+                
+                Group {
+                  Image(systemName: "arrow.up.right.square")
+                    Link(animal.name,destination: (URL(string: animal.link) ?? URL(string: "https://wikipedia.org"))!)
+                }
+            }//: HSTACK
+        }//: BOX
+    }
+}
+
+// MARK: - PREVIEW
+struct ExternalWeblink_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        ExternalWeblinkView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
+
