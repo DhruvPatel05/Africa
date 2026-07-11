1f9fff0ffd911d5142d799810a1820c9c2c9e4b1
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-10 21:52:20 -0700

    VideoModel Added.

---
+++ Africa/Africa/Model/VideoModel.swift
@@ -0,0 +1,16 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Dhruv Patel on 09/07/26.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    
+    let id: String
+    let name: String
+    let headline: String
+    
+}
