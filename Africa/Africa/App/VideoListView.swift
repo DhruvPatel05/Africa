229ddae61b7df1b81d8fb5b6dab876d20ad917e9
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-11 22:10:33 -0700

    VideoListView UI updated.

--- Africa/Africa/App/VideoListView.swift
+++ Africa/Africa/App/VideoListView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - PROPERTIES
+    
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Video List")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItem(video: item)
+                        .padding(.vertical,8)
+                } //: LOOP
+            }//: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos",displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action : {
+                        // Shuffle Videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        }// : NAVIGATION
     }
 }
 
