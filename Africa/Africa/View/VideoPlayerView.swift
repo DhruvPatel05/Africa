6effe09febb104682a48e33c6b0aa6a84cf30efd
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-12 22:39:46 -0700

    VideoPlayerView Added.

---
+++ Africa/Africa/View/VideoPlayerView.swift
@@ -0,0 +1,26 @@
+//
+//  VideoPlayerView.swift
+//  Africa
+//
+//  Created by Dhruv Patel on 12/07/26.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+// MARK: - PREVIEW
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoPlayerView()
+    }
+}
+
