26316922a192558c16392535b4912b62cf617f3f
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-14 22:37:48 -0400

    VideoPlayer View Updated.

--- Africa/Africa/View/VideoPlayerView.swift
+++ Africa/Africa/View/VideoPlayerView.swift
@@ -10,10 +10,25 @@ import AVKit
 
 struct VideoPlayerView: View {
     // MARK: - PROPERTIES
-    
+    var videoSelcted: String
+    var videoTitle: String
     // MARK: - BODY
     var body: some View {
-        VideoPlayer(player: playVideo(fileName: "cheetah", fileFormat: "mp4"))
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelcted, fileFormat: "mp4")) {
+                Text(videoTitle)
+            }
+            .overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32,height:32)
+                    .padding(.top,6)
+                    .padding(.horizontal,8),alignment: .topLeading
+                    )
+        }//: VSTACk
+        .accentColor(.accentColor)
+        .navigationBarTitle(videoTitle,displayMode: .inline)
     }
 }
 
@@ -21,7 +36,7 @@ struct VideoPlayerView: View {
 // MARK: - PREVIEW
 struct VideoPlayerView_Previews: PreviewProvider {
     static var previews: some View {
-        VideoPlayerView()
+        VideoPlayerView(videoSelcted: "lion", videoTitle: "Lion")
     }
 }
 
