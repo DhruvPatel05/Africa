57d096d237700fc109aea5e48f2405e3bccf33e9
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-15 22:00:22 -0400

    Mapview Region and coordinate added.

--- Africa/Africa/App/MapView.swift
+++ Africa/Africa/App/MapView.swift
@@ -6,14 +6,25 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286,longitude:16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+        
+    }
+    // MARK: - BODY
     var body: some View {
         Text("Map")
     }
 }
 
-
+// MARK: - PREVIEW
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView().previewDevice("iPhone 11 Pro")
