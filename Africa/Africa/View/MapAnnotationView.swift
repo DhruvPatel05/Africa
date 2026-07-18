175c4652dbfef32acc06f9c149eb5558d43fee08
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-18 09:44:20 -0400

    Custom MapAnnotationview Updated.

--- Africa/Africa/View/MapAnnotationView.swift
+++ Africa/Africa/View/MapAnnotationView.swift
@@ -11,16 +11,32 @@ import SwiftUI
 struct MapAnnotationView: View {
     //MARK: - PROPERTIES
     var location: NationalParkLoaction
-    
+    @State private var animation: Double = 0.0
     
     //MARK: - BODY
     var body: some View {
-        Image(location.image)
-            .resizable()
-            .scaledToFit()
-            .frame(width: 48,height: 48,alignment: .center)
-            .clipShape(Circle())
-    } //:ZSTACK
+        ZStack {
+            Circle()
+                .fill(Color.accentColor)
+                .frame(width:54,height:54,alignment: .center)
+            
+            Circle()
+                .stroke(Color.accentColor,lineWidth: 2)
+                .frame(width: 52,height: 52,alignment: .center)
+                .scaleEffect(1 + CGFloat(animation))
+                .opacity(1 - animation)
+            Image(location.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48,height: 48,alignment: .center)
+                .clipShape(Circle())
+        } //:ZSTACK
+        .onAppear {
+            withAnimation(Animation.easeOut(duration: 2).repeatForever(autoreverses: false)) {
+                animation = 1
+            }
+        }
+    }
 }
 
 //MARK: - PREVIEW
