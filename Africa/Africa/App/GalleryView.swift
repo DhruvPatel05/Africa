67c3f8645c59bbba188aa3dcc1227f9c0bcd6955
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-21 13:47:10 -0700

    Slider aded for changing the size of the animal logo and the smooth animation added for transition.

--- Africa/Africa/App/GalleryView.swift
+++ Africa/Africa/App/GalleryView.swift
@@ -13,6 +13,7 @@ struct GalleryView: View {
     // MARK: - PROPERTIES
     @State private var selectedAnimal: String = "lion"
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    
     //SIMPLE GRID DEFINITION
 //    let gridLayout:[GridItem] = [
 //        GridItem(.flexible()),
@@ -20,7 +21,15 @@ struct GalleryView: View {
 //        GridItem(.flexible())
 //    ]
     // EFIICIENT GRID DEFINITION
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    // DYNAMIC GRID LAYOUT
+    @State private var gridLayout:[GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch(){
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
     // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
@@ -31,6 +40,13 @@ struct GalleryView: View {
                     .scaledToFit()
                     .clipShape(Circle())
                     .overlay(Circle().stroke(Color.white,lineWidth: 8))
+                // MARK: - SLIDER
+                Slider(value: $gridColumn,in:2...4,step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { value in
+                        gridSwitch()
+                    })
+                
                 // MARK: - GRID
                 LazyVGrid(columns:gridLayout,alignment: .center,spacing: 10) {
                     ForEach(animals) { item in
@@ -44,6 +60,10 @@ struct GalleryView: View {
                             }
                     } //: LOOP
                 }//: GRID
+                .animation(.easeIn)
+                .onAppear(perform: {
+                    gridSwitch()
+                })
             }//: VSTACK
             .padding(.horizontal,10)
             .padding(.vertical,50)
