3edc222b3f20394e98d0358beee5a136828117b0
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-22 10:15:25 -0700

    Contentview updated for grid view option on navigationbar.

--- Africa/Africa/App/ContentView.swift
+++ Africa/Africa/App/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
    // MARK: - PROPERTIES
     let animals :[Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
     
+    @State private var isGridViewActive: Bool = false
     // MARK: - BODY
     var body: some View {
         NavigationView {
@@ -22,15 +24,42 @@ struct ContentView: View {
                     NavigationLink(destination: AnimalDetailView(animal: animal)){
                         AnimalListItemView(animal: animal)
                     }//: LINK
-                }
+                }//: LOOP
             }//: LIST
             .navigationBarTitle("Africa",displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing:16) {
+                     //LIST
+                        Button(action: {
+                           print("List view is activated")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        // GRID
+                        Button(action: {
+                            print("Grid view is activated")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    }//: HSTACK
+                }//: BUTTONs
+            }//: TOOLBAR
         }//: NAVIGATION
     }
 
    
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().previewDevice("iPhone 11 Pro")
